5c3483b4e2424ea2589a9da5f9b6a317a63a658b
Allyson Vinicius <allyson_vinicios1@hotmail.com>
2024-01-07 17:07:56 -0300

    Desafio concluido.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "0912", modelo: "Modelo 1", imei: "123456", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "3456", modelo: "Modelo 2", imei: "232323", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
 // TODO: Realizar os testes com as classes Nokia e Iphone
